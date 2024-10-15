091fc6825c6199c265ef926ebd47e4c6df361913
Ava618 <adwoodcock100@student.hbuhsd.edu>
2024-10-15 15:17:25 -0700

    made car go left and right

--- Prototype 1/Assets/Scrips/PlayerControler.cs
+++ Prototype 1/Assets/Scrips/PlayerControler.cs
@@ -5,6 +5,9 @@ public class PlayerControler : MonoBehaviour
 
 {
     public float speed = 10f;
+    public float rotationSpeed = 45f;
+
+    private Vector3 input;
 
     // Start is called before the first frame update
     void Start()
@@ -13,8 +16,13 @@ public class PlayerControler : MonoBehaviour
     }
 
     // Update is called once per frame
+
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-    }
+        input.z = Input.GetAxis( "Vertical");
+        input.x = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * input.z);
+        transform.Rotate(input.z * input.x * rotationSpeed * Time.deltaTime * Vector3.up);
+    } 
 }
